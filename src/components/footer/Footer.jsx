589b322fcfdc9a54ed3adb6f179f432f3f1dece7
zhaobowen <bowen.zhao@chiefclouds.com>
2021-03-29 15:28:46 +0800

    切分组件

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Layout, Typography} from 'antd';
+
+export const Footer = () => {
+    return (
+        <Layout.Footer>
+            <Typography.Title level={3} style={{'textAlign': 'center'}} >Test-Test</Typography.Title>
+        </Layout.Footer>
+    )
+}
